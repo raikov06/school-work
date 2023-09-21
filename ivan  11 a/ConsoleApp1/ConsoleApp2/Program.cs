15109004066d7a18ea47cb48273d8ffbb12a5f10
neshto <neshto>
2023-09-21 11:34:10 +0300

    new works

---
+++ ivan  11 a/ConsoleApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp2
+{
+    internal class StartUP
+    {
+        static void Main()
+        {
+            int rentOfHall = int.Parse(Console.ReadLine());
+            decimal statuesValue = rentOfHall * 0.7m;
+            decimal ceteringValue = rentOfHall * 0.7m;
+            decimal soundValue = rentOfHall * 0.7m;
+            decimal totalCost = rentOfHall * 0.7m;
+            Console.WriteLine($"otgovorut e {totalCost:F2}");
+        }
+        private static void Zadacha2()
+        {
+            int rentOfHall = int.Parse(Console.ReadLine());
+        }
+}
---
+++ ivan  11 a/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+
+            Dice d1 = new Dice();
+            Dice d2 = new Dice();
+
+            d1.Sides = 7;
+            d2.Sides = 3;
+            Console.WriteLine("Br na stranite na zara e: " + d1.Sides);
+            Console.WriteLine("Br na stranite na zara e: " + d2.Sides);
+        }
+
+
+    }
+
+    public class Dice
+    {
+        int sides;
+        public int Sides { get => sides; set => sides = 2 * value; }
+    }
+}
